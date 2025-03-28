552aaa020f6cf9503126333aceb0e68564b6308f
beartang2 <limeyengel@gmail.com>
2025-03-28 19:10:41 +0900

    VR setting & Player fix
    Init VR setting
    - Controller connecting
    - Changed Player composition

--- Assets/Script/Network_Scripts/NetworkUI.cs
+++ Assets/Script/Network_Scripts/NetworkUI.cs
@@ -6,27 +6,29 @@ using UnityEngine;
 
 public class NetworkUI : MonoBehaviour
 {
-    //[SerializeField] private Button Server_Button;
-    [SerializeField] private Button Host_Button;
+    [SerializeField] private Button Server_Button;
+    //[SerializeField] private Button Host_Button;
     [SerializeField] private Button Client_Button;
 
     private void Awake()
     {
-        /*
         //Lamda expression / Delegate
         Server_Button.onClick.AddListener(() => {
             NetworkManager.Singleton.StartServer();
+            Server_Button.enabled = true;
         });
-        */
 
+        /*
         //Lamda expression / Delegate
         Host_Button.onClick.AddListener(() => {
             NetworkManager.Singleton.StartHost();
         });
+        */
 
         //Lamda expression / Delegate
         Client_Button.onClick.AddListener(() => {
             NetworkManager.Singleton.StartClient();
+            Client_Button.enabled = true;
         });
     }
 }
