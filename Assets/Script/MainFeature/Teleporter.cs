970d104bcb8255d890a26f2f011dd37cae4f7228
beartang2 <limeyengel@gmail.com>
2025-05-28 05:06:26 +0900

    fixed 5:06

--- Assets/Script/MainFeature/Teleporter.cs
+++ Assets/Script/MainFeature/Teleporter.cs
@@ -45,7 +45,7 @@ public class Teleporter : MonoBehaviour, IResettable
             StartCoroutine(ReactivateTeleportersAfterDelay(4f)); // 3~5초 조절 가능
         }
 
-        if (!isTeleported && (other.CompareTag("keyObjects")|| other.CompareTag("commonObjects")))
+        if (!isTeleported && (other.tag.Contains("keyObject")|| other.tag.Contains("commonObject")))
         {
             Vector3 newPos = arrivePosObj.transform.position;
             newPos.y += yOffset;
